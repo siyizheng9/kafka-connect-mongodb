974ddb71a0cf4e30bb06eaecf5a93a9631bfe18d
hpgrahsl <hpgrahsl@users.noreply.github.com>
2017-06-23 10:11:14 -0500

    cleanup and config changes

--- src/test/java/at/grahsl/kafka/connect/mongodb/end2end/ContainerTest.java
+++ src/test/java/at/grahsl/kafka/connect/mongodb/end2end/ContainerTest.java
@@ -17,15 +17,19 @@
 package at.grahsl.kafka.connect.mongodb.end2end;
 
 import at.grahsl.kafka.connect.mongodb.data.avro.TweetMsg;
+import com.mongodb.MongoClient;
+import com.mongodb.MongoClientURI;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import static com.mongodb.client.model.Filters.*;
 import okhttp3.*;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.clients.producer.RecordMetadata;
+import org.bson.Document;
 import org.junit.ClassRule;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
 import org.junit.platform.runner.JUnitPlatform;
 import org.junit.runner.Description;
 import org.junit.runner.RunWith;
@@ -33,105 +37,136 @@ import org.testcontainers.containers.DockerComposeContainer;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Properties;
 
 @RunWith(JUnitPlatform.class)
 public class ContainerTest {
 
-    public static String COMPOSE_FILE = "src/test/resources/docker/compose-env.yml";
+    public static final String DOCKER_COMPOSE_FILE = "src/test/resources/docker/compose-env.yml";
+    public static final String DEFAULT_COMPOSE_SERVICE_SUFFIX = "_1";
 
-    public static String KAFKA_BROKER_SERVICE = "kafkabroker_1";
-    public static int KAFKA_BROKER_PORT = 9092;
+    public static final String SINK_CONNECTOR_CONFIG = "src/test/resources/config/sink_connector.json";
 
-    public static String KAFKA_CONNECT_SERVICE = "kafkaconnect_1";
-    public static int KAFKA_CONNECT_PORT = 8083;
+    //TODO: read these host:port settings from .yml compose file
 
-    public static String KAFKA_SCHEMA_REG_SERVICE = "schemaregistry_1";
-    public static int KAFKA_SCHEMA_REG_PORT = 8081;
+    public static final String KAFKA_BROKER = "kafkabroker";
+    public static final int KAFKA_BROKER_PORT = 9092;
 
-    public static String MONGODB_SERVICE = "mongodb_1";
+    public static final String KAFKA_CONNECT = "kafkaconnect";
+    public static final int KAFKA_CONNECT_PORT = 8083;
+
+    public static final String SCHEMA_REGISTRY = "schemaregistry";
+    public static final int SCHEMA_REGISTRY_PORT = 8081;
+
+    public static final String MONGODB = "mongodb";
     public static int MONGODB_PORT = 27017;
+
+    public static long PROCESSING_DELAY_TIME_MILLIS = 5_000L;
+
+    private static MongoClientURI MONGODB_CLIENT_URI;
+    private static MongoClient MONGO_CLIENT;
+    private static MongoDatabase MONGO_DATABASE;
+
+    private static KafkaProducer<String, TweetMsg> PRODUCER;
 
     @ClassRule
     public static DockerComposeContainer CONTAINER_ENV =
-            new DockerComposeContainer(new File(COMPOSE_FILE))
-                    .withExposedService(KAFKA_BROKER_SERVICE,KAFKA_BROKER_PORT)
-                    .withExposedService(KAFKA_CONNECT_SERVICE,KAFKA_CONNECT_PORT)
-                    .withExposedService(KAFKA_SCHEMA_REG_SERVICE,KAFKA_SCHEMA_REG_PORT)
-                    .withExposedService(MONGODB_SERVICE,MONGODB_PORT)
+            new DockerComposeContainer(new File(DOCKER_COMPOSE_FILE))
+                    .withExposedService(KAFKA_BROKER+DEFAULT_COMPOSE_SERVICE_SUFFIX,KAFKA_BROKER_PORT)
+                    .withExposedService(KAFKA_CONNECT+DEFAULT_COMPOSE_SERVICE_SUFFIX,KAFKA_CONNECT_PORT)
+                    .withExposedService(SCHEMA_REGISTRY +DEFAULT_COMPOSE_SERVICE_SUFFIX, SCHEMA_REGISTRY_PORT)
+                    .withExposedService(MONGODB+DEFAULT_COMPOSE_SERVICE_SUFFIX,MONGODB_PORT)
             ;
 
     @BeforeAll
-    public static void setup() {
+    public static void setup() throws IOException {
         CONTAINER_ENV.starting(Description.EMPTY);
-    }
+        MONGODB_CLIENT_URI = new MongoClientURI(
+                "mongodb://"+ MONGODB+":"+MONGODB_PORT+"/kafkaconnect"
+        );
+        MONGO_CLIENT = new MongoClient(MONGODB_CLIENT_URI);
+        MONGO_DATABASE = MONGO_CLIENT.getDatabase(MONGODB_CLIENT_URI.getDatabase());
 
-    @Test
-    @DisplayName("test producing record(s) to kafka and check resulting document(s) in mongodb sink")
-    public void firstBasicTestE2E() throws IOException {
-
-        //TODO: read this from config file
-        String config = "{\"name\": \"e2e-test-mongo-sink\",\"config\": {\"connector.class\": \"at.grahsl.kafka.connect.mongodb.MongoDbSinkConnector\",  \"topics\": \"e2e-test-topic\",  \"mongodb.connection.uri\": \"mongodb://mongodb:27017/kafkaconnect?w=1&journal=true\",  \"mongodb.document.id.strategy\": \"at.grahsl.kafka.connect.mongodb.processor.id.strategy.ProvidedInValueStrategy\",  \"mongodb.collection\": \"e2e-test-collection\"}}";
-
-        registerMongoDBSinkConnector(config);
-
-        //TODO: read demo data from test files
-        produceKafkaAvroRecord();
-
-        //TODO: read back from MongoDB and verify results
-        System.out.println("TODO: read back some data ...");
-    }
-
-    private static void produceKafkaAvroRecord() {
         Properties props = new Properties();
-        props.put("bootstrap.servers","kafkabroker:9092");
+        props.put("bootstrap.servers",KAFKA_BROKER+":"+KAFKA_BROKER_PORT);
         props.put("key.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
         props.put("value.serializer", "io.confluent.kafka.serializers.KafkaAvroSerializer");
-        props.put("schema.registry.url","http://schemaregistry:8081");
+        props.put("schema.registry.url","http://"+SCHEMA_REGISTRY+":"+ SCHEMA_REGISTRY_PORT);
+        PRODUCER = new KafkaProducer<>(props);
 
-        KafkaProducer<String, TweetMsg> producer = new KafkaProducer<>(props);
+        String config = new String(Files.readAllBytes(Paths.get(SINK_CONNECTOR_CONFIG)));
+        registerMongoDBSinkConnector(config);
+    }
 
-        for(int i = 0; i < 50; i++) {
+    @RepeatedTest(value=100, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @DisplayName("producing avro records to kafka topic")
+    public void produceKafkaAvroRecords(RepetitionInfo info) {
 
-            TweetMsg tweet = TweetMsg.newBuilder()
-                    .setId$1(123456789000L+i)
-                    .setText("test tweet "+(i+1)+": end2end testing apache kafka <-> mongodb sink connector is fun!")
-                    .setHashtags(Arrays.asList(new String[]{"t"+i,"kafka","mongodb","testing"}))
-                    .build();
+        int tweetCnt = info.getCurrentRepetition();
 
-            ProducerRecord<String, TweetMsg> record = new ProducerRecord<>("e2e-test-topic", tweet);
+        TweetMsg tweet = TweetMsg.newBuilder()
+                .setId$1(tweetCnt)
+                .setText("test tweet "+(tweetCnt)+": end2end testing apache kafka <-> mongodb sink connector is fun!")
+                .setHashtags(Arrays.asList(new String[]{"t"+tweetCnt,"kafka","mongodb","testing"}))
+                .build();
 
-            System.out.println(LocalDateTime.now() + " producer sending -> " + tweet.toString());
+        ProducerRecord<String, TweetMsg> record = new ProducerRecord<>("e2e-test-topic", tweet);
+        System.out.println(LocalDateTime.now() + " producer sending -> " + tweet.toString());
 
-            producer.send(record, (RecordMetadata r, Exception e) -> {
-                if (e != null) {
-                    System.out.println("error: producing tweet "+tweet+" failed");
-                    e.printStackTrace();
-                }
-            });
+        PRODUCER.send(record, (RecordMetadata r, Exception exc) -> {
+            assertNull(exc, () -> "unexpected error while sending: " + tweet
+                                        + " | exc: "+exc.getMessage()
+            );
+        });
 
+    }
+
+    @RepeatedTest(value = 100, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @DisplayName("looking up expected records in MongoDB collection")
+    public void verifyMongoDbSinkRecords(RepetitionInfo info) {
+
+        //defer manually wait for data propagation before first read back
+        if(info.getCurrentRepetition() == 1) {
+            deferExecutionToWaitForDataPropagation(PROCESSING_DELAY_TIME_MILLIS);
         }
 
-        producer.close();
+        int tweetCnt = info.getCurrentRepetition();
+        MongoCollection<Document> col = MONGO_DATABASE.getCollection("e2e-test-collection");
+        List<Document> docs = new ArrayList<>();
+
+        Document found = col.find(eq(tweetCnt)).first();
+        assertNotNull(found, "document having _id="+tweetCnt+" is expected to be found");
+        System.out.println("found = " + found);
+
     }
 
     private static void registerMongoDBSinkConnector(String configuration) throws IOException {
-
         RequestBody body = RequestBody.create(
                 MediaType.parse("application/json"), configuration
         );
 
         Request request = new Request.Builder()
-                .url("http://kafkaconnect:8083/connectors")
+                .url("http://"+KAFKA_CONNECT+":"+KAFKA_CONNECT_PORT+"/connectors")
                 .post(body)
                 .build();
 
+        System.out.println("sending -> "+configuration);
         Response response = new OkHttpClient().newCall(request).execute();
         assert(response.code() == 201);
         response.close();
+    }
 
+    private static void deferExecutionToWaitForDataPropagation(long millis) {
+        System.out.println("giving the processing some time to propagate the data...");
+        try {
+            Thread.sleep(PROCESSING_DELAY_TIME_MILLIS);
+        } catch (InterruptedException e) {}
     }
 
 }
